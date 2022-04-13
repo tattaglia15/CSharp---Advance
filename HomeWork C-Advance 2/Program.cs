de17079834e5731772ba05df6618fb13a44414df
tattaglia15 <96620459+tattaglia15@users.noreply.github.com>
2022-04-13 16:15:13 +0200

    Add files via upload

---
+++ HomeWork C-Advance 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Class_01___Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Please enter Date example mm/dd/yy");
+                string enterDate = Console.ReadLine();
+                DateTime dates;
+                bool validate = DateTime.TryParse(enterDate, out dates);
+                if (!validate)
+                {
+                    Console.WriteLine("The Date is Incorrect, Try Again!");
+                }
+                else
+                {
+                    Console.WriteLine("The Date is Correct");
+                }
+                if (
+                dates.Month == 1 && dates.Day == 1 ||
+                dates.Month == 1 && dates.Day == 7 ||
+                dates.Month == 4 && dates.Day == 20 ||
+                dates.Month == 5 && dates.Day == 1 ||
+                dates.Month == 8 && dates.Day == 8 ||
+                dates.Month == 10 && dates.Day == 12 ||
+                dates.Month == 10 && dates.Day == 23 ||
+                dates.Month == 12 && dates.Day == 8
+                )
+                {
+                    Console.WriteLine("These are non working days also");
+                }
+                else if (dates.Day == 1 || dates.Day == 2 || dates.Day == 3 || dates.Day == 4 || dates.Day == 5)
+                {
+                    Console.WriteLine("Working Days");
+                }
+                else if (dates.Day == 6 || dates.Day == 7)
+                {
+                    Console.WriteLine("These are weekends");
+                }
+                else
+                {
+                    Console.WriteLine("You must enter from 1 to 7 day if you want to check if is work or weekend day");
+                }  
+
+            //switch(dates.Day)
+            //{
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("Working Days");
+            //        break;
+            //    case 6:
+            //    case 7:
+            //        Console.WriteLine("These are Weekends");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You must enter from 1 to 7 day to check if is working day or not");
+            //        break;
+            //}
+
+
+        }
+    }
+}
---
+++ HomeWork- C-Advance - 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork___C_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Names:");
+            List<string> inputs = new List<string>();
+
+            bool myInput = true;
+            while(myInput)
+            {
+                string input = Console.ReadLine();
+                if(input == "x")
+                {
+                    Console.WriteLine("You Enter x To Shut Down Program");
+                    break;
+                }
+              inputs.Add(input);
+            }
+            Console.WriteLine("Please Enter a sentence:");
+            string[] sentence = Console.ReadLine().Split(' ');
+            int counter = 0;
+            foreach(string input in inputs)
+            {
+                foreach(string sentence2 in sentence)
+                {
+                    if(sentence2.ToLower() == input.ToLower())
+                    {
+                        counter++;  
+                    }
+                };
+            }
+            Console.WriteLine($"The number of names contained in sentence is : {counter}");
+        }
+    }
+}
